dcb2d7a0f22adf8ddd679934dd60ca82a5134d90
romzz.git <romzz.patel@gmail.com>
2021-11-22 20:20:45 -0500

    changed login page

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -9,7 +9,7 @@ import { useQuery, useMutation } from '@apollo/client';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(QUERY_ME);
-  const userData = data?.me || data?.user || {};
+  const userData = data?.me || {};
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
@@ -24,6 +24,7 @@ const SavedBooks = () => {
       await removeBook({ variables: { bookId } })
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
+      window.location.replace('/saved');
     } catch (err) {
       console.error(err);
     }
@@ -43,12 +44,12 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
+          {userData.savedBooks?.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
                 {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
